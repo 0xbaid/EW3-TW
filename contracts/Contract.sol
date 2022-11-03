335971f185c1c152fd5c2a8e05bb30f1c3724b57
obddd <obaidulhaq20@gmail.com>
2022-11-03 23:32:13 +0500

    completed

--- contracts/Contract.sol
+++ contracts/Contract.sol
@@ -2,21 +2,36 @@
 pragma solidity ^0.8.0;
 
 import "@thirdweb-dev/contracts/base/ERC721Base.sol";
+import "@thirdweb-dev/contracts/extension/PermissionsEnumerable.sol";
 
-contract Contract is ERC721Base {
+contract Contract is ERC721Base, PermissionsEnumerable {
+    mapping(uint256 => uint256) powerlevel;
 
-      constructor(
+    constructor(
         string memory _name,
         string memory _symbol,
         address _royaltyRecipient,
         uint128 _royaltyBps
-    )
-        ERC721Base(
-            _name,
-            _symbol,
-            _royaltyRecipient,
-            _royaltyBps
-        )
-    {}
+    ) ERC721Base(_name, _symbol, _royaltyRecipient, _royaltyBps) {
+        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+    }
 
+    function setPowerLevel(uint256 _tokenId, uint256 _powerlevel)
+        external
+        onlyRole(DEFAULT_ADMIN_ROLE)
+    {
+        powerlevel[_tokenId] = _powerlevel;
+    }
+
+    function mintTo(address _to, string memory _tokenURI)
+        public
+        virtual
+        override
+    {
+        uint256 tokenId = nextTokenIdToMint();
+
+        super.mintTo(_to, _tokenURI);
+
+        powerlevel[tokenId] = tokenId;
+    }
 }
